58b5c9c7b62ddb94894d169def05516d2c2c9d99
pauloenriquez <133067132+pauloenriquez@users.noreply.github.com>
2024-01-09 15:04:07 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='opencv',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        'opencv-python',
+        # other dependencies
+    ],
+)
